404722ac83af08b545c6414df39cbf114d6baa7d
jhonl <jhonl@LAPTOP-7QIA8QJI>
2022-02-08 07:40:30 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int c = 0;
+double d = 0.5;
+char e = 'f';
+string g = "bazuco";
+float f = 234.089898f;
+
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(g);
+Console.WriteLine("esta es una variable de tipo float: " + f);
+Console.Write("la concatenación se hace con el signo '+'  \n salto de linea ");
+Console.Write("hola me llamo jhon  \n arroz con huevo ");
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            if (5 > 4) ;
+            {
+                Console.WriteLine("d es mayor que c");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/pruebas C#.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int c = 0;
+double d = 0.5;
+char e = 'f';
+string g = "bazuco con coca cola";
+float f = 234.089898f;
+
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(g);
+Console.WriteLine("esta es una variable de tipo float: " + f);
+Console.Write("la concatenación se hace con el signo '+'  \n salto de linea ");
+
+// Console.Write("hola me llamo jhon  \n arroz con huevo ");
+
